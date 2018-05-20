00c9662b3a29d59f1d19707d031f351bd8cefdae
Shad"TheJavaSlayer"Nolen <Shadnolen@msn.com>
2018-05-20 12:03:02 -0700

    UserInterface
    Working on User Interface Designs.

---
+++ UXD_Island/header.php
@@ -0,0 +1,69 @@
+<html>
+<head>
+    <link href="css/style.css" type="textcss" rel="stylesheet">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://use.fontawesome.comreleases/v5.0.10css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">    
+    <link rel="icon" href="imagesfavicon.ico">       
+    </head>
+<!---- header ---->
+ <header> 
+    <nav>        
+        <img id="logo" src="images/logo2.png">   
+        
+       <ul>
+            <li><a href="index.html">Home</a></li>
+            <li class="dropdown">
+                <a href="javascript:void(0)" class="dropbtn">Where to Stay</a>
+                <div class="dropdown-content">
+                    <a href ="rental/hotel.html">Hotel Deals</a> 
+                    <a href ="rental/villas.html">Villas</a> 
+                    <a href ="rental/bb.html">B&amp;B</a> 
+                    <a href ="rental/guest.html">Guesthouses</a> 
+                    <a href ="faqfaq.html">What to know</a> 
+                </div>
+                
+            <li class="dropdown">
+                <a href="javascript:void(0)" class="dropbtn">Travel Guide</a>
+                <div class="dropdown-content">
+                    <a href="traveltrans.html">Getting to the Island</a>
+                    <a href="traveltrans.html">Local Travel</a>
+                    <a href="ent/beach.html">Top Beaches</a>
+                    <a href="ent/golf.html">Golfing</a>
+                    <a href="enttours.html">Tours</a>                     
+                    <a href="faq/bank.html">Banking&#44; Currency &amp; Exchange rate</a>
+                    <a href="faqfaq.html">Things to know</a>
+                </div>
+                
+            <li class="dropdown">
+                <a href="javascript:void(0)" class="dropbtn">Island Cuisine</a>
+                <div class="dropdown-content">                   
+                    <a href="ent/night.html">Local Spirts &amp; Beer</a>
+                    <a href="entisland.html">Island Flavor</a>                
+                    <a href="ent/sea.html">From the Sea</a>                    
+                </div>      
+             
+           <li class="dropdown">
+                <a href="javascript:void(0)" class="dropbtn">Entertainment</a>
+                <div class="dropdown-content">
+                    <a href = "ent/arts.html">Arts</a> 
+                    <a href = "entevents.html">Events</a>    
+                    <a href = "faq/history.html">Historical sites</a> 
+                    <a href = "ent/music.html">Music</a> 
+                    <a href="ent/shop.html">Shopping</a>
+                    <a href="ent/night.html">Nightlife</a>
+                </div> 
+           
+           
+               <li class="dropdown">
+                <a href="javascript:void(0)" class="dropbtn">Groups</a>
+                <div class="dropdown-content">
+                    <a href = "faq/gov.html">Meetings</a> 
+                    <a href = "faq/gov1.html">Incentives</a>    
+                    <a href = "faq/gov1.html">Events</a> 
+                    <a href = "faq/gov1.html">Conferences</a> 
+                    <a href = "faq/gov1.html">FAQ</a> 
+                </div>               
+        </ul>
+    </nav>
+ </header>
+     <!---- end header ---->
